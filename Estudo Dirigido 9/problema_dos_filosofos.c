cd5dbadd6fa505c71d3b06a3ac35ca4e5c55343d
Eduardo <eduardocalandrini@hotmail.com>
2016-10-05 15:29:31 -0300

    Estudo Dirigido 9 e 10 - Problema dos filosofos - Problea da Soma da Matriz

---
+++ Estudo Dirigido 9/problema_dos_filosofos.c
@@ -0,0 +1,12 @@
+/******************************************
+ * Aluno: Eduardo Calandrini Rocha da Costa
+ * Matricula: 13/0059595
+ * Programacao Concorrente - Prof Alchieri
+ *******************************************/
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+ #include <unistd.h>
+ #include <pthread.h>
+ #include <semaphore.h>
